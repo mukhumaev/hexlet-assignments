1b4e550b7e8cb08be0361aa6700f7844f812cc40
@hexlet/cli <support@hexlet.io>
2022-12-26 15:12:25 +0300

    submit ruby-basics-ru/collections

--- ruby-basics-ru/collections/lib/build_query_string.rb
+++ ruby-basics-ru/collections/lib/build_query_string.rb
@@ -6,8 +6,8 @@ def build_query_string(query)
   res = ''
   sorted_query = query.sort_by { |key| key.to_s }.to_h
 
-  sorted_query.each_pair do |k, v|
-    res += "#{k}=#{v}&"
+  sorted_query.each_pair do |key, val|
+    res += "#{key}=#{val}&"
   end
 
   res.chop
