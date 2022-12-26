3f725c9a5a0b6cdefea22ae6686605bc65bad948
@hexlet/cli <support@hexlet.io>
2022-12-26 03:11:42 +0300

    submit ruby-basics-ru/basics

--- ruby-basics-ru/basics/lib/fibonacci.rb
+++ ruby-basics-ru/basics/lib/fibonacci.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 # BEGIN
-def fibonnacci(num)
-  return nil if num < 0
+def fibonacci(num)
+  return nil if num.negative?
   return num if num <= 1
+
   fibonacci(num - 1) + fibonacci(num - 2)
 end
 # END
--- ruby-basics-ru/basics/lib/reverse.rb
+++ ruby-basics-ru/basics/lib/reverse.rb
@@ -3,11 +3,11 @@
 # BEGIN
 def reverse(str)
   length = 0
-  while true
+  loop do
     length += 1
     break if str[length].nil?
   end
-  
+
   result = ''
   while length.positive?
     length -= 1
